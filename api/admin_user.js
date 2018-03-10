0e5bb729f627d8752145695db30f4665a56901af
Shiien <935697492@qq.com>
2018-03-10 13:46:58 +0800

    增加展示某一个role的用户

--- api/admin_user.js
+++ api/admin_user.js
@@ -11,9 +11,37 @@ router.post('/', (req, res) => {
 })
 
 router.get('/list',async (req, res) => {
+
   const result=await db.query(`select * from user_role`);
   res.ok(result.rows)
 })
+
+router.get('/list_role',async(req,res)=>
+{
+  const find_user_role=req.body.user_role
+  try {
+    const tmp = await db.query(`select role_id from user_role where title = ($1)`, [find_user_role])
+  }
+  catch (err)
+  {
+    res.fail('1',err)
+    throw(err)
+  }
+  //console.log(tmp.rows[0].role_id.toString())
+  value='{'+tmp.rows[0].role_id.toString()+'}'
+  //console.log(value)
+  try {
+    const result = await  db.query(`select * from user_info where user_role @> $1`, [value])
+    res.ok(result)
+  }
+  catch (err)
+  {
+    res.fail('1',err)
+    throw (err)
+  }
+
+})
+
 router.post('/create_role',check_perm(MANAGE_ROLE), async (req, res) => {
   const keys = ['role_title', 'role_description', 'perm', 'negative']
   const values = [req.body.title, req.body.description,  req.body.perm,req.body.negative]

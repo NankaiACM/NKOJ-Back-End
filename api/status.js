e3779528b72c68ec8bd6825fe8c8636a464eab7f
Shiien <935697492@qq.com>
2018-03-24 22:40:15 +0800

    show code

--- api/status.js
+++ api/status.js
@@ -1,5 +1,5 @@
 const router = require('express').Router()
-const db = require('./db')
+const db = require('../database/db')
 const fs = require('fs')
 const redis = require('redis')
 const client = redis.createClient()
@@ -11,6 +11,9 @@ const getAsync = promisify(client.get).bind(client)
 const multer = require('multer')
 const path = require('path')
 const md5 = require('../lib/md5')
+const {SOLUTION_PATH} = require('../config/basic')
+const {GET_CODE_SELF}=require('../lib/perm-check')
+const {check_perm}=require('../lib/perm-check')
 
 router.post('/list',  async (req, res) => {
   'use strict'
@@ -29,5 +32,31 @@ router.post('/list',  async (req, res) => {
   }
   return res.fail(1, 'No solutions!')
 })
+
+router.post('/code', check_perm(GET_CODE_SELF),  async (req, res) => {
+  'use strict'
+  const keys = ['integer']
+  const values = [req.body.solutionId]
+  const rules = []
+  const form = {}
+  let checkResult
+  checkResult = check(keys, values, rules, form)
+  if(checkResult) return res.fail(1, checkResult)
+  const queryString = 'SELECT user_id FROM solutions WHERE solution_id = $1 '
+  const result = await db.query(queryString, values)
+  if(result.rows.length > 0){
+    if(req.session.user===result.rows[0].user_id) {
+      if (fs.existsSync(`${SOLUTION_PATH}/${values[0]}`)) {
+        res.sendFile(`${SOLUTION_PATH}/${values[0]}`)
+      } else {
+        res.fail(1,'No solution\'s file')
+      }
+    }
+    else res.fail(1,'other\'s code')
+  }
+  return res.fail(1, 'No solutions!')
+})
+
+
 
 module.exports = router

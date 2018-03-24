e5744581355d29d19235e9793de7d9e413ae0453
Sun Yi-Ran <sunrisefox@qq.com>
2018-03-25 00:30:09 +0900

    Merged.

--- api/admin.js
+++ api/admin.js
@@ -1,9 +1,12 @@
 const router = require('express').Router()
+
+const email = require('./admin_email')
 const role = require('./admin_role')
 const user = require('./admin_user')
 
-const {check_perm, MANAGE_ROLE} = require('../lib/perm-check')
+const {check_perm, MANAGE_ROLE, SUPER_ADMIN} = require('../lib/perm-check')
 
+router.use('/email', check_perm(SUPER_ADMIN), email)
 router.use('/role', check_perm(MANAGE_ROLE), role)
 router.use('/user', check_perm(MANAGE_ROLE), user)
 

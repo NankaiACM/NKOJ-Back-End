00bf72cbcdad1deefa18e215a2e6bd5257a073c1
Shiien <935697492@qq.com>
2018-03-24 22:41:09 +0800

    show code

--- config/basic.js
+++ config/basic.js
@@ -2,5 +2,6 @@ module.exports = {
   BASE_URL: 'http://111.231.98.20:8000',
   AVATAR_PATH: '/var/www/upload',
   PROBLEM_PATH: 'problem',
-  CONTEST_PATH: 'contest'
+  CONTEST_PATH: 'contest',
+  SOLUTION_PATH:'../public/solution'
 }

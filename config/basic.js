a21199f311d846a8537ae885f63b6af578d853b9
baitieyi <baitieyi@163.com>
2018-03-27 19:36:11 +0800

    BASE_URL

--- config/basic.js
+++ config/basic.js
@@ -1,7 +1,7 @@
 module.exports = {
-  BASE_URL: 'http://111.231.98.20:8000',
+  BASE_URL: 'https://vampire.rip',
   AVATAR_PATH: '/var/www/upload',
   PROBLEM_PATH: 'problem',
   CONTEST_PATH: 'contest',
-  SOLUTION_PATH:'public/solution'
+  SOLUTION_PATH: 'public/solution'
 }

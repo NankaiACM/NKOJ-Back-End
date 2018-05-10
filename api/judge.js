d1114cf47bb1ec549e13ad9dada4c84db81a0c6c
DSaurus <2238454358@qq.com>
2018-05-10 13:16:53 +0800

    judge基本对接完成

--- api/judge.js
+++ api/judge.js
@@ -1,56 +1,93 @@
 const router = require('express').Router()
 const fs = require('fs')
+const path = require("path")
 const db = require('../database/db')
 const ws = require('ws')
 const {SOLUTION_PATH} = require('../config/basic')
+const {PROBLEM_DATA_PATH} = require('../config/basic')
+const {DATA_BASE} = require('../config/basic')
+Date.prototype.format = require('../lib/dateFormat')
 
-router.get('/', async (req, res) => {
+router.post('/', async (req, res) => {
   'use strict'
-  const webstorm = new ws('ws://192.168.99.100:8000')
-  webstorm.on('open', function open () {
-    webstorm.send('./')
-    webstorm.send('1')
-    webstorm.send('1001')
-    webstorm.send('0')
-    webstorm.send('1000')
-    webstorm.send('2560000')
-    webstorm.send('3')
-    webstorm.send('0')
-    webstorm.send('1')
-  })
-  webstorm.on('close', function close () {
-    res.ok('!!!')
-  })
-  /*
-  if(!req.session.user) return res.fatal(401)
-  const problem = req.body.p
-  const lang = req.body.lang
+  if(!req.session.user) return res.fail(3, 'Not login!')
+  const problem = req.body.pid
+  const lang = 1
   const code = req.body.code
   const user = req.session.user
   const ip = req.ip
   let ipaddr_id = ''
 
-  const queryString = 'INSERT INTO solutions (user_id, problem_id, language, ipaddr_id)' +
-    ' VALUES ($1, $2, $3, $4) RETURNING solution_id'
+  // const initString = 'INSERT INTO solution_status (status_id, msg_short, msg_cn, msg_en)' +
+  //   'VALUES ($1, $2, $3, $4)'
+  // await db.query(initString, [1, "ac", "ac", "ac"])
+
+  const queryString = 'INSERT INTO solutions (user_id, problem_id, language, ipaddr_id, status_id)' +
+    ' VALUES ($1, $2, $3, $4, 1) RETURNING solution_id'
   let ipquery = 'SELECT * FROM ipaddr WHERE ipaddr = $1'
 
   let result = await db.query(ipquery, [ip])
-  if(result.rows.length > 0){
+  if (result.rows.length > 0) {
     ipaddr_id = result.rows[0].ipaddr_id
   } else {
     ipquery = 'INSERT INTO ipaddr (ipaddr) VALUES ($1) RETURNING ipaddr_id'
     result = await db.query(ipquery, [ip])
     ipaddr_id = result.rows[0].ipaddr_id
   }
+  console.log('ipaddr_id')
   let langString = 'cpp'
   db.query(queryString, [user, problem, lang, ipaddr_id]).then(suc => {
-    fs.writeFile(`${SOLUTION_PATH}/${suc.rows[0].solution_id}.${langString}`, code, err => {
-      if(!err) return res.ok('Submit Successfully!')
-      else return res.fail(2, err)
+    const solution_id = suc.rows[0].solution_id
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/temp`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/temp`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/exec`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/exec`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/cmperr`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/cmperr`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/execout`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/execout`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/diff`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/diff`)
+    if (!fs.existsSync(`${SOLUTION_PATH}/${solution_id}/codes`)) fs.mkdirSync(`${SOLUTION_PATH}/${solution_id}/codes`)
+    if(fs.existsSync(`${SOLUTION_PATH}/${solution_id}/codes`))
+    fs.open(`${SOLUTION_PATH}/${solution_id}/codes/${solution_id}.${langString}`, "w", err => {
+      if (err) {
+        console.log(err)
+        res.fail(2, err)
+      }
+      else {
+        fs.writeFile(`${SOLUTION_PATH}/${solution_id}/codes/${solution_id}.${langString}`, code, err => {
+          if (!err) {
+            const webstorm = new ws('ws://127.0.0.1:8888')
+            webstorm.on('open', function open() {
+              webstorm.send(DATA_BASE + "/")
+              webstorm.send(solution_id)
+              webstorm.send(problem)
+              webstorm.send('1')
+              webstorm.send('1001')
+              webstorm.send('2560000')
+              webstorm.send('1')
+              webstorm.send('0')
+              webstorm.send('1')
+            })
+            webstorm.on('close', function close() {
+              fs.readFile(`${SOLUTION_PATH}/${solution_id}/temp/${solution_id}_error`, "utf-8", (err, data) => {
+                if(err) res.fail(2, err)
+                else res.ok(data)
+              })
+              fs.readFile(`${SOLUTION_PATH}/${solution_id}/temp/${solution_id}_result`, "utf-8", (err, data) => {
+                if(err) console.log(err)
+                else {
+                  const alterString = "UPDATE solutions SET status_id = $1 WHERE solution_id = $2"
+                  db.query(alterString, [data, solution_id])
+                }
+              })
+            })
+            //return res.ok('Submit Successfully!')
+          }
+          else return res.fail(2, err)
+        })
+      }
     })
   }, err => {
     res.fail(1, err)
-  })*/
+  })
 })
 
 module.exports = router
--- api/status.js
+++ api/status.js
@@ -11,69 +11,118 @@ const getAsync = promisify(client.get).bind(client)
 const multer = require('multer')
 const path = require('path')
 const md5 = require('../lib/md5')
-const {matchedData} = require('express-validator/filter')
-const {validationResult} = require('express-validator/check')
-const check = require('../lib/form-check')
+//const check = require('../lib/form-check')
+const { matchedData} = require('express-validator/filter');
+const {validationResult}=require('express-validator/check')
+const check=require('../lib/form-check1')
 const {SOLUTION_PATH} = require('../config/basic')
-const {GET_CODE_SELF} = require('../lib/permission')
-const {require_perm} = require('../lib/permission')
+const {GET_CODE_SELF}=require('../lib/perm-check')
+const {check_perm}=require('../lib/perm-check')
 
-router.post('/list', [check.queryleft, check.queryright], async (req, res) => {
+router.get('/init',async (req, res) => {
   'use strict'
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    res.fail(1, errors.array())
+  const initString = 'INSERT INTO solution_status (status_id, msg_short, msg_cn, msg_en) ' +
+    'VALUES ($1, $2, $3, $4)'
+  await db.query(initString, ['100', 'RU', '运行中', 'Running'])
+  await db.query(initString, ['101', 'CE', '编译错误', 'Compile Error'])
+  await db.query(initString, ['120', 'CI', '正在编译', 'Compiling'])
+  await db.query(initString, ['102', 'WA', '答案错误', 'Wrong Answer'])
+  await db.query(initString, ['103', 'RE', '运行错误', 'Runtime Error'])
+  await db.query(initString, ['104', 'MLE', '内存超限', 'Memory Limit Exceed'])
+  await db.query(initString, ['105', 'TLE', '时间超限', 'Time Limit Exceed'])
+  await db.query(initString, ['106', 'OLE', '输出超限', 'Output Limit Exceed'])
+  await db.query(initString, ['107', 'AC', '答案正确', 'Accepted'])
+  await db.query(initString, ['108', 'PE', '格式错误', 'Presentation Error'])
+  await db.query(initString, ['109', 'FL', '函数调用不合法', 'Function Limit Exceed'])
+  await db.query(initString, ['118', 'SE', '未知错误', 'System Error'])
+
+  return res.ok('init complete!')
+})
+
+router.post('/list',[check.queryleft,check.queryright],  async (req, res) => {
+  'use strict'
+  /*const keys = ['integer', 'integer']
+  const values = [req.body.queryleft, req.body.queryright]
+  const rules = []
+  const form = {}
+  let checkResult
+  checkResult = check(keys, values, rules, form)
+  if(checkResult) return res.fail(1, checkResult)*/
+  const errors = validationResult(req);
+  if(!errors.isEmpty())
+  {
+    res.fail(1,errors.array())
     return
   }
-  const checkres = matchedData(req)
-  const values = [checkres.queryleft, checkres.queryright]
-  const queryString = 'SELECT * FROM status WHERE solution_id BETWEEN $1 AND $2'
+  const checkres = matchedData(req);
+  const values=[checkres.queryleft, checkres.queryright]
+  const queryString = 'SELECT s.*, s2.* FROM solutions AS s INNER JOIN solution_status AS s2' +
+    ' ON s.status_id = s2.status_id' +
+    ' WHERE s.solution_id BETWEEN $1 AND $2' +
+    ' ORDER BY s.solution_id'
   const result = await db.query(queryString, values)
-  if (result.rows.length > 0) {
+  if(result.rows.length > 0){
     return res.ok(result.rows)
   }
   return res.fail(1, 'No solutions!')
 })
 
-router.post('/code', require_perm(GET_CODE_SELF), check.solutionId, async (req, res) => {
+router.post('/code', check_perm(GET_CODE_SELF), check.solutionId, async (req, res) => {
   'use strict'
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    res.fail(1, errors.array())
+  /*const keys = ['integer']
+  const values = [req.body.solutionId]
+  const rules = []
+  const form = {}
+  let checkResult
+  checkResult = check(keys, values, rules, form)
+  if(checkResult) return res.fail(1, checkResult)*/
+  const errors = validationResult(req);
+  if(!errors.isEmpty())
+  {
+    res.fail(1,errors.array())
     return
   }
-  const checkres = matchedData(req)
-  const values = [checkres.solutionId]
+  const checkres = matchedData(req);
+  const values=[checkres.solutionId]
   const queryString = 'SELECT user_id FROM solutions WHERE solution_id = $1 '
   const result = await db.query(queryString, values)
-  if (result.rows.length > 0) {
-    if (req.session.user === result.rows[0].user_id) {
+  if(result.rows.length > 0){
+    if(req.session.user===result.rows[0].user_id) {
       if (fs.existsSync(path.resolve(SOLUTION_PATH, values[0]))) {
         res.sendFile(path.resolve(SOLUTION_PATH, values[0]))
       } else {
-        res.fail(1, 'No solution\'s file')
+        res.fail(1,'No solution\'s file')
       }
     }
-    else res.fail(1, 'other\'s code')
+    else res.fail(1,'other\'s code')
   }
   return res.fail(1, 'No solutions!')
 })
 
-router.get('/:problemId', check.problemId, async (req, res) => {
+router.get('/:problemId',check.problemId,async (req, res) => {
   'use strict'
+  /*const problemId = req.params.problemId
+  const keys = ['integer']
+  const values = [problemId]
+  const rules = []
+  const form = {}
+  let checkResult
+  checkResult = check(keys, values, rules, form)
+  if(checkResult) return res.fail(1, checkResult)*/
 
-  const errors = validationResult(req)
-  if (!errors.isEmpty()) {
-    res.fail(1, errors.array())
+  const errors = validationResult(req);
+  if(!errors.isEmpty())
+  {
+    res.fail(1,errors.array())
     return
   }
-  const checkres = matchedData(req)
-  const values = [checkres.solutionId]
+  const checkres = matchedData(req);
+  const values=[checkres.solutionId]
   const queryString = 'SELECT * FROM status WHERE problem_id = $1 ORDER BY time,memory'
   const result = await db.query(queryString, values)
-  if (result.rows.length > 0) {
-    return res.ok(result.rows)
-  }
+  if(result.rows.length > 0){
+      return res.ok(result.rows)
+    }
   else return res.fail(1, 'No solutions!')
 })
 

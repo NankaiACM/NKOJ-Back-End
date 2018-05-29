7a1b77a49bb117694d50477571ff128ad673d7d7
Sun Yi-Ran <sunrisefox@qq.com>
2018-05-29 15:22:19 +0900

    支持 rejudge

--- api/judge.js
+++ api/judge.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const db = require('../database/db')
 const ws = require('ws')
 const {DATA_BASE} = require('../config/basic')
-const {require_perm} = require('../lib/permission')
+const {require_perm, REJUDGE_ALL} = require('../lib/permission')
 const fc = require('../lib/form-check')
 const {getSolutionStructure, unlinkTempFolder} = require('../lib/judge')
 
@@ -65,6 +65,56 @@ router.post('/', require_perm(), fc.all(['pid', 'lang', 'code']), async (req, re
       // res.fail(500, e)
     }
     unlinkTempFolder(solution_id)
+  })
+})
+
+router.get('/rejudge/:sid', require_perm(REJUDGE_ALL), async (req, res) => {
+  const sid = Number(req.params.sid) || undefined
+  if (!Number.isInteger(sid)) return res.fail(422)
+  const struct = getSolutionStructure(sid)
+  const result = await db.query('UPDATE solutions SET status_id = 100 WHERE solution_id = $1 RETURNING problem_id', [sid])
+  if (!result.rows.length) return res.fail(404)
+
+  const pid = result.rows[0].problem_id
+  let problem_info = await db.query('SELECT time_limit, memory_limit, cases, special_judge::integer, detail_judge::integer FROM problems WHERE problem_id = $1', [pid])
+  if (!problem_info.rows) return res.fail(500, 'problem is not available?')
+
+  // TODO:
+  const langString = 'cpp'
+
+  const row = problem_info.rows[0]
+  const time_limit = row.time_limit
+  const memory_limit = row.memory_limit
+  const cases = row.cases
+  const special_judge = row.special_judge
+  const detail_judge = row.detail_judge
+
+  const socket = new ws('ws://127.0.0.1:8888')
+  socket.on('error', function (err) {
+    throw err
+  })
+  socket.on('message', function (msg) {
+    console.log(msg)
+  })
+  socket.on('open', function open () {
+    socket.send([DATA_BASE, sid, pid, langString, time_limit, memory_limit, cases, special_judge, detail_judge].join('\n'))
+  })
+  socket.on('close', async function close () {
+    try {
+      const code_length = fs.statSync(`${struct.file.code_base}${langString}`).size
+      const result = fs.readFileSync(struct.file.result, 'utf8').split('\n')[0]
+      const time = fs.readFileSync(struct.file.time, 'utf8').split('\n')[0]
+      const memory = fs.readFileSync(struct.file.memory, 'utf8').split('\n')[0]
+      const detail = fs.readFileSync(struct.file.detail, 'utf8')
+      const compile_info = fs.readFileSync(struct.file.compile_info, 'utf8')
+
+      await db.query('UPDATE solutions SET status_id = $1, "time" = $2, "memory" = $3, code_size = $4 WHERE solution_id = $5', [result, time, memory, code_length, sid])
+      if (result !== '') res.ok({solution_id: sid, time, memory, result, detail, compile_info, code_length})
+      else res.fail(500, 'seems something wrong, contact admin...')
+    } catch (e) {
+      res.fail(500, e.stack || e)
+    }
+    unlinkTempFolder(sid)
   })
 
 })

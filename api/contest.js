5d9e2b7dee621cecd6df1fd709876a40476b5baf
DSaurus <2238454358@qq.com>
2018-12-20 14:26:41 +0800

    Add finished contest api
    Just copy and paste...

--- api/contest.js
+++ api/contest.js
@@ -29,6 +29,31 @@ router.get('/:cid', fc.all(['cid']), async (req, res) => {
   res.ok({...basic, problems: problems.rows, file})
 })
 
+router.get('/:cid/finished', fc.all(['cid']), async (req, res) => {
+  'use strict'
+  const cid = req.fcResult.cid
+
+  const result = await db.query(
+    'SELECT *, lower(during) AS start, upper(during) AS end FROM contests WHERE contest_id = $1', [cid]
+  )
+  if (result.rows.length === 0) return res.fail(404)
+
+  const basic = result.rows[0]
+
+  const problems = await db.query(
+    'SELECT problem_id, submit_ac as ac, submit_all as all, title, special_judge, detail_judge, level' +
+    ' FROM contest_problems LEFT JOIN problems WHERE contest_id = $1', [cid]
+  )
+
+  let file
+  try {
+    file = fs.readFileSync(`${CONTEST_PATH}/${cid}.md`, 'utf8')
+  } catch (e) {
+
+  }
+  res.ok({...basic, problems: problems.rows, file})
+})
+
 router.get('/:cid/user', fc.all(['cid']), async (req, res) => {
   'use strict'
   const cid = req.fcResult.cid

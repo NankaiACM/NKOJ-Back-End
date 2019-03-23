34c6ea07fab008ba787fa56442d72b7c06544981
SunriseFox <sunrisefox@qq.com>
2019-03-23 21:17:13 +0800

    update compatibility

--- api/admin/admin_problem_spj.js
+++ api/admin/admin_problem_spj.js
@@ -1,11 +1,12 @@
 const router = require('express').Router()
 
 const {getProblemStructure, getSolutionStructure, unlinkTempFolder} = require('../../lib/judge')
-const {PROBLEM_SPJ_PATH, TEMP_PATH} = require('../../config/basic')
+const {PROBLEM_SPJ_PATH, TEMP_PATH, DATA_BASE} = require('../../config/basic')
 const multer = require('multer')
 const path = require('path')
 const fs = require('fs')
-const ext = require('../../lib/extension')
+const language_ext = require('../../lib/extension')
+const {spawn} = require('../../lib/spawn')
 
 router.post('/:pid', (req, res, next) => {
   if (!Number.isInteger(Number(req.params.pid)))
@@ -21,10 +22,10 @@ const upload = multer({
   })
 })
 
-router.post('/:pid', upload.single('file'), (req, res, next) => {
+router.post('/:pid', upload.single('file'), async (req, res, next) => {
   const pid = req.params.pid
   const language = req.body.lang || 1
-  const ext = ext[language]
+  const ext = language_ext[language]
   const spj = getProblemStructure(pid).path.spj
   const filename = `${spj}.${ext}`
   console.log(filename);
@@ -33,6 +34,18 @@ router.post('/:pid', upload.single('file'), (req, res, next) => {
     if (!req.body.data) res.fail(400, 'neither file nor data was supplied')
     fs.writeFileSync(filename, req.body.data)
   }
+
+  const config = {
+    "debug": true, // [false]
+    "base_path": DATA_BASE, // [/mnt/data]
+    "lang": language_ext[ext], // <必填>，可以是 c, c++, javascript, python, go
+    "code": filename, // [<base_path>/judge/<pid>.<ext>]
+    "target": spj  // [<base_path>/judge/<pid>]
+  }
+
+  fs.writeFileSync(`${spj}.config`, JSON.stringify(config))
+  await spawn('docker', ['exec', '-i', 'judgecore', './compiler', `${spj}.config`]);
+
   res.ok()
 })
 

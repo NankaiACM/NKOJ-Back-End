08f39267b77e2c1f6350f43652f5361ff2aa7db5
Alexhaoge <1810064@mail.nankai.edu.cn>
2021-04-26 19:19:21 +0800

    complete api

--- api/admin/admin_user.js
+++ api/admin/admin_user.js
@@ -1,8 +1,9 @@
 const router = require('express').Router()
-const {require_perm, MANAGE_ROLE, SUPER_ADMIN} = require('../../lib/permission')
-const db = require('../../database/db')
-const fc = require('../../lib/form-check')
-const session = require('../../lib/session')
+import {require_perm, MANAGE_ROLE, SUPER_ADMIN} from '../../lib/permission'
+import db from '../../database/db'
+import fc from '../../lib/form-check'
+import session  from '../../lib/session'
+import {encrypt, genRawStr} from '../../lib/rsa'
 
 // TODO: test
 router.post('/add', fc.all(['nickname', 'password', 'email', 'words', 'count']), async (req, res) => {
@@ -75,11 +76,36 @@ router.get('/logout/:who', async (req, res) => {
 router.get('/addmulti/:cid/:num', fc.all(['cid']), async (req, res, next) => {
   const cid = parseInt(req.params.cid)
   const num = parseInt(req.params.num);
-  const cret = await db.query(`SELECT contest_id, private FROM contests WHERE contest_id = ${cid}`)
-  if (cret.rows.length === 0) return res.fail(404, 'contest not found') 
-  if (!cret.rows[0].private) 
+  let ret = await db.query(`SELECT contest_id, private FROM contests WHERE contest_id = ${cid}`)
+  if (ret.rows.length === 0) return res.fail(404, 'contest not found') 
+  if (!ret.rows[0].private) 
     return res.fail(422, 'A public contest is not applicable for contest users')
-  
+  ret = await db.query(
+    'SELECT split_part(nickname, \'_\', 2) AS mid FROM users WHERE nickname LIKE $1 ORDER BY nickname DESC LIMIT 1',
+    ['c' + cid + '_%']
+  )
+  const begin = ret.rows.length === 0 ? 1 : parseInt(ret.rows[0].mid) + 1 
+  let insertArr = Array(num), resUser = new Array(num)
+  for (let i = 0; i < num; i++) {
+    const name = `c${cid}_${i+begin}`
+    const pass_raw = genRawStr(8)
+    const pass_en = encrypt(pass_raw)
+    insertArr[i] = `(
+      '${name}','${pass_en}','${name}@dummy.nankai.edu.cn',
+      3, '{1, 4}', 'NKU', 'A', '::ffff:127.0.0.1')`
+    resUser[i] = {username: name, password: pass_raw}
+  }
+  const insertPrefix = 'INSERT INTO users (nickname, password, email, gender, role, school, words, ipaddr) VALUES'
+  const insertSuffix = ' RETURNING user_id'
+  try {
+    ret = await db.query(insertPrefix + insertArr.join(',') + insertSuffix)
+    for (let i = 0; i < num; i++) {
+      resUser[i].user_id = ret.rows[i].user_id      
+    }
+    return res.ok({resUser})
+  } catch (e) {
+    return res.fail(520, e)
+  }
 })
 
 module.exports = router
